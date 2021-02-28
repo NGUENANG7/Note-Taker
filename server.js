acf26c3bbb863a5a26d00801a009f654d12db4f0
Gregory Nguenang <njethur@gmail.com>
2021-02-28 10:49:34 -0600

    Updating broken code

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const server = require('http').Server(app);
+
 const PORT = process.env.PORT || 8000;
 
 //Sets up the Express app to handle data parsing
@@ -11,9 +11,14 @@ app.use(express.json());
 
 
 // Sets up the Express app to find the public folder
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname,'./assets/css')));
+app.use(express.static(__dirname));
+
 
 require("./routes/apiRoute")(app);
 require("./routes/htmlRoute")(app);
 
-server.listen(PORT, () => {});
+
+app.listen(PORT, () => {
+    console.log(`App listening on PORT: ${PORT}`);
+});
