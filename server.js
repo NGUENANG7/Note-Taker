4811633f98ec11fa9ddb2b0c294d47bd02b5f42d
Gregory Nguenang <njethur@gmail.com>
2021-02-28 18:37:56 -0600

    Update

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const app = express();
-let PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 3030;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
+require("./routes/apiRoute")(app);
+require("./routes/htmlRoute")(app);
 
 app.listen(PORT, function () {
-	console.log("App listening on PORT " + PORT);
+    console.log("App listening on PORT " + PORT);
 });
