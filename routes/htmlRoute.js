f1e767329e3543a5a4a8c383e9e7981785c725ea
Gregory Nguenang <njethur@gmail.com>
2021-03-01 00:55:25 -0600

    update

--- routes/htmlRoute.js
+++ routes/htmlRoute.js
@@ -1,15 +1,15 @@
 const path = require("path");
 const fs = require("fs");
 
-    router.get('/', (req, res) => {
+    router.get('/', (_req, res) => {
         res.sendFile(path.join(__dirname, 'public/index.html'));
     });
 
-    router.get('/notes', (req, res) => {
+    router.get('/notes', (_req, res) => {
         res.sendFile(path.join(__dirname, 'public/notes.html'));
     });
 
-    router.get('/api/notes', (req, res) => {
+    router.get('/api/notes', (_req, res) => {
         const notes = JSON.parse(fs.readFileSync('./db/db.json'));
         res.json(notes);
     });
